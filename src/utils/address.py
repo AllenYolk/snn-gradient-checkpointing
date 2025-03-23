98767f575f85289ce7d12ab9ba52dbe186d443d4
AllenYolk <allen.yfhuang@gmail.com>
2025-03-24 04:36:10 +0800

    feat: add address reference tools

---
+++ src/utils/address.py
@@ -0,0 +1,52 @@
+from typing import DefaultDict
+import inspect
+import gc
+import torch
+
+
+def garbage_collection():
+    collected = gc.collect()
+    print(f"Garbage collection: collected {collected} objects.")
+
+
+def is_address_referenced_by_tensor(addr):
+    for obj in gc.get_objects():
+        if isinstance(obj, torch.Tensor) and obj.data_ptr() == addr:
+            return True
+    return False
+
+
+def get_all_addresses_referenced_by_tensor(verbose=False):
+    id_to_name = {}
+    current_frame = inspect.currentframe().f_back
+
+    while current_frame:
+        frame_name = current_frame.f_code.co_name
+        locals_dict = current_frame.f_locals
+        for obj_name, obj in locals_dict.items():
+            if isinstance(obj, torch.Tensor):
+                id_to_name[id(obj)] = f"{frame_name}.{obj_name}"
+        current_frame = current_frame.f_back
+
+    addr_to_tensor = DefaultDict(list)
+    for obj in gc.get_objects():
+        if isinstance(obj, torch.Tensor):
+            d = {"tensor": obj, "name": id_to_name.get(id(obj), None)}
+            addr_to_tensor[obj.data_ptr()].append(d)
+
+    if verbose:
+        title = "Addresses Referenced by Tensors"
+        print("=" * 20, title, "=" * 20)
+        for addr, tensor_info_list in addr_to_tensor.items():
+            print(f"<{addr}>", end="")
+            n_indent = len(str(addr)) + 2
+            for i, tensor_info in enumerate(tensor_info_list):
+                real_indent = 0 if i == 0 else n_indent
+                x = tensor_info["tensor"]
+                obj_name = tensor_info["name"]
+                info_str = f'-> "{obj_name}" ' if obj_name else "-> "
+                info_str += f"{list(x.size())} ({x.dtype})"
+                print(" " * real_indent, info_str)
+        print("=" * (42 + len(title)))
+
+    return addr_to_tensor
---
+++ test/test_address_referenced.py
@@ -0,0 +1,76 @@
+import sys
+
+sys.path.insert(0, "./src")
+
+import torch
+import torch.nn as nn
+
+from models import *
+from utils import *
+
+
+def test_lif_address_referenced():
+    T = 1000
+    N = 64
+    C = 200
+    x = torch.randn(T, N, C) + 0.6
+    print(f"network input: {x.data_ptr()}")
+
+    net = nn.Sequential(
+        nn.ReLU(),
+        HandWrittenLIFNode(),
+        nn.ReLU(),
+    )
+    x.requires_grad = True
+
+    s = net(x)
+    loss = s.sum()
+    loss.backward()
+
+    print(f"network output: {s.data_ptr()}")
+    print(f"loss: {loss.data_ptr()}")
+
+
+def test_linear_lif_address_referenced():
+    T = 1000
+    N = 64
+    C = 200
+    x = torch.randn(T, N, C) + 0.6
+    print(f"network input: {x.data_ptr()}")
+
+    net = nn.Sequential(
+        LinearLIF(
+            proj=nn.Linear(C, C),
+            neuron=HandWrittenLIFNode(),
+            spike_compressor=Uint8SpikeCompressor(),
+        ),
+        LinearLIF(
+            proj=nn.Linear(C, C),
+            neuron=HandWrittenLIFNode(),
+            spike_compressor=BooleanSpikeCompressor(),
+        ),
+        LinearLIF(
+            proj=nn.Linear(C, C),
+            neuron=HandWrittenLIFNode(),
+            spike_compressor=BitSpikeCompressor(),
+        ),
+        LinearLIF(
+            proj=nn.Linear(C, C),
+            neuron=HandWrittenLIFNode(),
+            spike_compressor=SparseSpikeCompressor(),
+        )
+    )
+    x.requires_grad = True
+
+    s = net(x)
+    loss = s.sum()
+    get_all_addresses_referenced_by_tensor(verbose=True)
+    loss.backward()
+
+    print(f"network output: {s.data_ptr()}")
+    print(f"loss: {loss.data_ptr()}")
+
+
+if __name__ == "__main__":
+    # test_lif_address_referenced()
+    test_linear_lif_address_referenced()
