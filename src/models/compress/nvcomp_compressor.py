bcfc7d1099ac059c39df8a8f716e3e21ccb04b7b
AllenYolk <allen.yfhuang@gmail.com>
2025-04-03 13:33:11 +0800

    feat: add nvcomp chunk support

--- src/models/compress/nvcomp_compressor.py
+++ src/models/compress/nvcomp_compressor.py
@@ -1,8 +1,7 @@
 try:
-    import abc
+    from typing import Sequence, Union
     import torch
     from nvidia import nvcomp
-    from ..amp import AUTOCAST_DTYPE, is_autocast_enabled
 
     NVCOMP_AVAILABLE = True
 
@@ -18,30 +17,67 @@ try:
     def torch_type_to_nvcomp_type(dtype):
         return NVCOMP_TYPE_DICT.get(dtype, dtype)
 
-    def nvcomp_compress(x: torch.Tensor, algorithm: str = "Zstd"):
+    DEFAULT_NVCOMP_CODEC_ALGORITHM = "Zstd"
+    DEFAULT_NVCOMP_CODEC_KWARGS = {}
+
+    NVCOMP_CHUNK_SIZE_BYTES = 16777216
+    NVCOMP_NEED_CHUNK = {
+        "Zstd": True,
+        "ANS": True,
+    }
+
+    # ===================== functional interface ======================
+    def nvcomp_compress(
+        x: torch.Tensor,
+        algorithm: str = DEFAULT_NVCOMP_CODEC_ALGORITHM,
+        **kwargs
+    ):
+        x_size_byte = x.numel() * x.element_size()
         x = torch.tensor((), device=x.device, dtype=torch.uint8).set_(
             x.untyped_storage(),
             x.storage_offset(),
-            (x.numel() * x.element_size(),),
+            (x_size_byte,),
         )
-        x = nvcomp.as_array(x)
+
+        if (
+            x_size_byte > NVCOMP_CHUNK_SIZE_BYTES and
+            NVCOMP_NEED_CHUNK[algorithm]
+        ):
+            x = torch.split(x, NVCOMP_CHUNK_SIZE_BYTES)
+            x = [nvcomp.as_array(chunk) for chunk in x]
+        else:
+            x = nvcomp.as_array(x)
+
+        additional_kwargs = DEFAULT_NVCOMP_CODEC_KWARGS
+        additional_kwargs.update(kwargs)
 
         return nvcomp.Codec(
             algorithm=algorithm,
             bitstream_kind=nvcomp.BitstreamKind.RAW,
+            **additional_kwargs,
         ).encode(x)
 
     def nvcomp_decompress(
-        x: nvcomp.Array,
+        x: Union[nvcomp.Array, Sequence[nvcomp.Array]],
         target_shape,
         target_dtype=torch.float32,
-        algorithm: str = "Zstd",
+        algorithm: str = DEFAULT_NVCOMP_CODEC_ALGORITHM,
+        **kwargs
     ):
+        additional_kwargs = DEFAULT_NVCOMP_CODEC_KWARGS
+        additional_kwargs.update(kwargs)
         x = nvcomp.Codec(
             algorithm=algorithm,
             bitstream_kind=nvcomp.BitstreamKind.RAW,
+            **additional_kwargs,
         ).decode(x)
-        x = torch.from_dlpack(x.to_dlpack())
+
+        if isinstance(x, nvcomp.Array):
+            x = torch.from_dlpack(x.to_dlpack())
+        else:
+            x = [torch.from_dlpack(chunk.to_dlpack()) for chunk in x]
+            x = torch.cat(x, dim=0)
+
         y = torch.tensor((), dtype=target_dtype, device=x.device)
         y = y.set_(
             x.untyped_storage(),
@@ -50,16 +86,23 @@ try:
         )
         return y.reshape(target_shape)
 
+    # ===================== class interface ======================
     class NvcompCompressor:
 
         def __init__(
-            self, algorithm: str = "Zstd", compressed_dtype=torch.uint8
+            self,
+            algorithm: str = DEFAULT_NVCOMP_CODEC_ALGORITHM,
+            compressed_dtype=torch.uint8,
+            **kwargs  # additional arguments for nvcomp.Codec
         ):
             self.algorithm = algorithm
             self.compressed_dtype = compressed_dtype
+            additional_kwargs = DEFAULT_NVCOMP_CODEC_KWARGS
+            additional_kwargs.update(kwargs)
             self.codec = nvcomp.Codec(
                 algorithm=algorithm,
                 bitstream_kind=nvcomp.BitstreamKind.RAW,
+                **additional_kwargs,
             )
 
         def compress(self, x: torch.Tensor) -> nvcomp.Array:
--- test/test_nvcomp.py
+++ test/test_nvcomp.py
@@ -5,7 +5,7 @@ sys.path.append("./src")
 import torch
 from nvidia import nvcomp
 
-from models.compress import nvcomp_compress, nvcomp_decompress, NvcompCompressor
+from models.compress import *
 
 
 def test_manual():
@@ -60,8 +60,67 @@ def test_manual():
     print("Restored equal to original?", torch.allclose(x_f32, x_dec_f32))
 
 
+def test_manual_chunk():
+    torch.manual_seed(2025)
+    x_f32 = torch.randn(32 * 128 * 128 * 11, device='cuda', dtype=torch.float32)
+    print("Original dtype:", x_f32.dtype)
+    print("Original size (bytes):", x_f32.numel() * 4)
+    print("Original data_ptr:", x_f32.data_ptr())
+    print("Original shape:", x_f32.shape)
+    print("Original size (elements):", x_f32.numel())
+    print("Original storage offset:", x_f32.storage_offset())
+
+    x_uint8 = torch.tensor((), device='cuda', dtype=torch.uint8).set_(
+        x_f32.untyped_storage(), x_f32.storage_offset(),
+        (x_f32.numel() * x_f32.element_size(),)
+    )
+    print("Shared uint8 dtype:", x_uint8.dtype)
+    print("Shared uint8 size:", x_uint8.numel())
+    print("Shared uint8 data_ptr:", x_uint8.data_ptr())
+
+    x_uint8_chunks = torch.split(x_uint8, NVCOMP_CHUNK_SIZE_BYTES)
+    print(f"Number of chunks: {len(x_uint8_chunks)}")
+    print("Chunk sizes (bytes):", [chunk.numel() for chunk in x_uint8_chunks])
+    x_nv_chunks = [nvcomp.as_array(chunk) for chunk in x_uint8_chunks]
+
+    codec = nvcomp.Codec(
+        algorithm="ANS", bitstream_kind=nvcomp.BitstreamKind.RAW
+    )
+    compressed_chunks = codec.encode(x_nv_chunks)
+    for i, compressed_chunk in enumerate(compressed_chunks):
+        print(
+            f"\tCompressed nvcomp size of chunk {i} (bytes):",
+            compressed_chunk.buffer_size
+        )
+
+    decompressed_chunks = codec.decode(compressed_chunks)
+    x_dec = [
+        torch.from_dlpack(chunk.to_dlpack()) for chunk in decompressed_chunks
+    ]
+    x_dec = torch.cat(x_dec, dim=0)
+    print("Decompressed size (bytes):", x_dec.numel() * 4)
+    print("Decompressed data_ptr:", x_dec.data_ptr())
+    print("Decompressed shape:", x_dec.shape)
+    print("Decompressed size (elements):", x_dec.numel())
+    print("Decompressed storage offset:", x_dec.storage_offset())
+
+    x_dec_f32 = torch.tensor((), dtype=torch.float32, device="cuda").set_(
+        x_dec.untyped_storage(),
+        x_dec.storage_offset(),
+        (x_dec.numel() // 4,),
+    )
+    print("x_dec_f32 data_ptr:", x_dec_f32.data_ptr())
+    print("x_dec_f32 dtype:", x_dec_f32.dtype)
+    print("x_dec_f32 size (bytes):", x_dec_f32.numel() * 4)
+    print("x_dec_f32 shape:", x_dec_f32.shape)
+    print("x_dec_f32 size (elements):", x_dec_f32.numel())
+    print("x_dec_f32 storage offset:", x_dec_f32.storage_offset())
+
+    print("Restored equal to original?", torch.allclose(x_f32, x_dec_f32))
+
+
 def test_function():
-    torch.manual_seed(2023)
+    torch.manual_seed(2025)
     x_f32 = torch.randn((31, 81), device='cuda', dtype=torch.float32)
     print("Original size (bytes):", x_f32.numel() * x_f32.element_size())
 
@@ -73,24 +132,45 @@ def test_function():
     print("Restored equal to original?", torch.allclose(x_f32, x_reconstruct))
 
 
+def test_function_chunk():
+    torch.manual_seed(2025)
+    x_f32 = torch.randn(
+        (32 * 128 * 128 * 11,),
+        device='cuda',
+        dtype=torch.float32,
+    )
+    print("Original size (bytes):", x_f32.numel() * x_f32.element_size())
+
+    x_comp = nvcomp_compress(x_f32, algorithm="Zstd")
+
+    x_reconstruct = nvcomp_decompress(x_comp, x_f32.shape, x_f32.dtype, "Zstd")
+
+    print("Restored equal to original?", torch.allclose(x_f32, x_reconstruct))
+
+
 def test_class():
     torch.manual_seed(2025)
-    x_f32 = torch.rand((31, 23), device='cuda', dtype=torch.float32)
-    x_i64 = (x_f32 > 0.8).to(torch.int64)
-    print("Original dtype:", x_i64.dtype)
-    print("Original size (bytes):", x_i64.numel() * x_i64.element_size())
+    x_f32 = torch.rand(
+        (32 * 128 * 128 * 8,),
+        device='cuda',
+        dtype=torch.float32,
+    )
+    print("Original dtype:", x_f32.dtype)
+    print("Original size (bytes):", x_f32.numel() * x_f32.element_size())
 
-    compressor = NvcompCompressor("Zstd", torch.uint8)
+    compressor = NvcompCompressor("ANS", torch.uint8)
 
-    x_comp = compressor.compress(x_i64)
+    x_comp = compressor.compress(x_f32)
     print("Compressed nvcomp size (bytes):", x_comp.buffer_size)
 
-    x_reconstruct = compressor.decompress(x_comp, x_i64.shape, x_i64.dtype)
+    x_reconstruct = compressor.decompress(x_comp, x_f32.shape, x_f32.dtype)
 
-    print("Restored equal to original?", torch.allclose(x_i64, x_reconstruct))
+    print("Restored equal to original?", torch.allclose(x_f32, x_reconstruct))
 
 
 if __name__ == "__main__":
-    # test_manual()
-    test_function()
-    test_class()
+    #test_manual()
+    test_manual_chunk()
+    #test_function()
+    test_function_chunk()
+    #test_class()
