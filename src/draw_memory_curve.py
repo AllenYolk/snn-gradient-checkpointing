87441273feb6ce1dcd10a42bada9830d32c8cb58
AllenYolk <allen.yfhuang@gmail.com>
2025-04-29 15:27:24 +0800

    fix: refine memory curve plotting script

--- src/draw_memory_curve.py
+++ src/draw_memory_curve.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-
+import numpy as np
 import matplotlib.pyplot as plt
 import scienceplots
 
@@ -8,6 +8,7 @@ plt.style.use(["science", "nature", "grid", "no-latex"])
 
 MB = 1024 * 1024
 W, H = plt.rcParams["figure.figsize"]
+current_palette = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 
 def get_curve(data_path):
@@ -19,33 +20,56 @@ def get_curve(data_path):
     backward_end_memory = data_dict["backward_end_memory"]
     backward_peak_memory = data_dict["backward_peak_memory"]
 
-    curve = []
+    fp_curve = []
+    bp_curve = []
     for k in forward_start_memory.keys():
-        curve.append(forward_start_memory[k] / MB)
-        curve.append(forward_peak_memory[k] / MB)
-        curve.append(forward_end_memory[k] / MB)
-    for k in reversed(backward_start_memory.keys()):
-        curve.append(backward_start_memory[k] / MB)
-        curve.append(backward_peak_memory[k] / MB)
-        curve.append(backward_end_memory[k] / MB)
-    return curve
+        fp_curve.append(forward_start_memory[k] / MB)
+        fp_curve.append(forward_peak_memory[k] / MB)
+        fp_curve.append(forward_end_memory[k] / MB)
+    for k in backward_start_memory.keys():
+        bp_curve.append(backward_start_memory[k] / MB)
+        bp_curve.append(backward_peak_memory[k] / MB)
+        bp_curve.append(backward_end_memory[k] / MB)
+    return fp_curve, bp_curve
+
+
+def plot_curve(ax, fp_curve, bp_curve, label, color):
+    ax.plot(
+        np.arange(len(fp_curve)),
+        fp_curve,
+        marker="o",
+        markersize=2,
+        label=label,
+        color=color
+    )
+    ax.plot(
+        np.arange(
+            len(fp_curve),
+            len(fp_curve) + len(bp_curve),
+        ),
+        bp_curve,
+        marker="d",
+        markersize=2,
+        color=color
+    )
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path_1", type=str)
-parser.add_argument("--data_path_2", type=str)
-parser.add_argument("--data_path_3", type=str)
+parser.add_argument("--data_path_2", type=str, default=None)
+parser.add_argument("--data_path_3", type=str, default=None)
 args = parser.parse_args()
 
-curve1 = get_curve(args.data_path_1)
-curve2 = get_curve(args.data_path_2)
-curve3 = get_curve(args.data_path_3)
+fp_curve1, bp_curve1 = get_curve(args.data_path_1)
+fp_curve2, bp_curve2 = get_curve(args.data_path_2)
+fp_curve3, bp_curve3 = get_curve(args.data_path_3)
 
 f, ax = plt.subplots(figsize=(W * 1.5, H))
-ax.plot(curve1, marker="o", markersize=2, label="BPTT")
-ax.plot(curve2, marker="o", markersize=2, label="G.C.")
-ax.plot(curve3, marker="o", markersize=2, label="G.C. + Bit")
+plot_curve(ax, fp_curve1, bp_curve1, "BPTT", current_palette[0])
+plot_curve(ax, fp_curve2, bp_curve2, "G.C.", current_palette[1])
+plot_curve(ax, fp_curve3, bp_curve3, "G.C.+1bit", current_palette[2])
 ax.set_ylabel("Memory Usage (MB)")
+ax.set_xticklabels([])
 ax.grid(alpha=0.3)
 ax.legend()
 plt.tight_layout()
