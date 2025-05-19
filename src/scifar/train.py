93018d61ba2bc39236a1591bc1228202ebaa7f09
AllenYolk <allen.yfhuang@gmail.com>
2025-05-20 04:07:50 +0800

    fix: refine lightning

--- src/scifar/train.py
+++ src/scifar/train.py
@@ -1,11 +1,8 @@
-import time
 import sys
-from tqdm import tqdm
 
 sys.path.append("./src")
 
 import torch
-import torch.nn.functional as F
 from utils import use_torch_npu
 
 npu_available = use_torch_npu()
@@ -14,12 +11,9 @@ if npu_available:
 else:
     print("NPU is not available.")
 
-from spikingjelly.activation_based import functional
 from lightning.pytorch.cli import LightningCLI
 
-from utils import ModelNameGenerator, AverageMeter
-from utils import accuracy, Lomo
-from modules import get_autocast_context
+from utils import Lomo
 import models as models
 from modules import ClassificationLightningModule
 from data_module import SCIFARDataModule
@@ -81,95 +75,9 @@ class SCIFARLightningModule(ClassificationLightningModule):
         return ([optimizer], [lr_scheduler])
 
 
-def train_step(
-    net, train_data_loader, optimizer, lr_scheduler, device, scaler,
-    current_epoch, total_epoch
-):
-    epoch_start_time = time.time()
-    net.train()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-    use_amp = scaler is not None
-
-    with tqdm(
-        train_data_loader,
-        desc=f"Epoch {current_epoch + 1}/{total_epoch}",
-        leave=False,
-        unit="batch"
-    ) as pbar:
-        for img, label in pbar:
-            img, label = img.float().to(device), label.to(device)
-            img = img.permute(3, 0, 1, 2)  # [W, N, C, H]; W acts as T
-
-            with get_autocast_context(use_amp):
-                y = net(img)
-                batch_loss = F.cross_entropy(y, label)
-
-            if use_amp:
-                scaler.scale(batch_loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                batch_loss.backward()
-                optimizer.step()
-            optimizer.zero_grad()
-
-            functional.reset_net(net)
-            prec1, prec5 = accuracy(y.data, label.argmax(1).data, topk=(1, 5))
-            losses.update(batch_loss.item(), label.size(0))
-            top1.update(prec1.item(), label.size(0))
-            top5.update(prec5.item(), label.size(0))
-
-            pbar.set_postfix({
-                "loss": losses.avg,
-                "top1_acc": top1.avg,
-                "top5_acc": top5.avg,
-            })
-
-    if lr_scheduler is not None:
-        lr_scheduler.step()
-
-    epoch_end_time = time.time()
-    epoch_time = epoch_end_time - epoch_start_time  # unit: second
-    return {
-        "loss": losses.avg,
-        "top1_acc": top1.avg,
-        "top5_acc": top5.avg,
-        "time": epoch_time
-    }
-
-
-def val_step(net, test_data_loader, device):
-    net.eval()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-
-    with torch.no_grad():
-        for img, label in test_data_loader:
-            img, label = img.float().to(device), label.to(device)
-            img = img.permute(3, 0, 1, 2)  # [W, N, C, H]; W acts as T
-
-            y = net(img)
-            batch_loss = F.cross_entropy(y, label)
-
-            # measure accuracy and record loss
-            functional.reset_net(net)
-            prec1, prec5 = accuracy(y.data, label.data, topk=(1, 5))
-            losses.update(batch_loss.item(), label.size(0))
-            top1.update(prec1.item(), label.size(0))
-            top5.update(prec5.item(), label.size(0))
-
-    return {
-        "loss": losses.avg,
-        "top1_acc": top1.avg,
-        "top5_acc": top5.avg,
-    }
-
-
 def main():
     cli = LightningCLI(SCIFARLightningModule, SCIFARDataModule, run=False)
+    print(cli.model)
     cli.trainer.fit(cli.model, datamodule=cli.datamodule)
 
 
