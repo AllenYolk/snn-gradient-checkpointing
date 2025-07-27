d726de5e99674420711799ea7cc707042d1e748d
AllenYolk <allen.yfhuang@gmail.com>
2025-07-27 22:20:30 +0800

    fix: add DH-LIF SHD

--- src/shd/models.py
+++ src/shd/models.py
@@ -28,7 +28,7 @@ class LinearPLIF(nn.Module):
         beta_initializer='uniform',
         beta_low=0,
         beta_high=4,
-        vth=0.5,
+        vth=1.,
     ):
         super().__init__()
         self.in_features = in_features
@@ -68,7 +68,7 @@ class LinearPLIFCheckpointing(BaseCheckpointingBlock):
         beta_initializer='uniform',
         beta_low=0,
         beta_high=4,
-        vth=0.5,
+        vth=1.,
         spike_compressor="NullSpikeCompressor",
     ):
         super().__init__()
@@ -210,6 +210,195 @@ class LinearOutputPLIFCheckpointing(nn.Module):
         )
 
 
+class LinearDHLIF(nn.Module):
+
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        alpha_initializer='uniform',
+        alpha_low=2,
+        alpha_high=6,
+        beta_initializer='uniform',
+        beta_low=0,
+        beta_high=4,
+        vth=1.,
+        branch=4,
+        mask_share=1
+    ):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.vth = vth
+        self.branch = branch
+
+        self.mask_share = mask_share
+        self.pad = (in_features//branch*branch + branch - in_features) % branch
+        self.dense = nn.Linear(in_features + self.pad, out_features * branch)
+
+        #sparsity
+        self.sparsity = 1 / branch
+        self.overlap = 1 / branch
+        self._alpha = nn.Parameter(torch.empty([self.out_features, branch]))
+        self._beta = nn.Parameter(torch.empty([self.out_features]))
+
+        self.create_mask()
+
+        if alpha_initializer == 'uniform':
+            nn.init.uniform_(self._alpha, alpha_low, alpha_high)
+        elif alpha_initializer == 'constant':
+            nn.init.constant_(self._alpha, alpha_low)
+
+        if beta_initializer == 'uniform':
+            nn.init.uniform_(self._beta, beta_low, beta_high)
+        elif beta_initializer == 'constant':
+            nn.init.constant_(self._beta, beta_low)
+
+    def create_mask(self):
+        input_size = self.in_features + self.pad  # the real input channels
+        mask = torch.zeros(self.out_features * self.branch, input_size)
+        for i in range(self.out_features // self.mask_share):
+            seq = torch.randperm(input_size)
+            for j in range(self.branch):
+                for k in range(self.mask_share):
+                    x = (i * self.mask_share + k) * self.branch + j
+                    y_start = j * input_size // self.branch
+                    y_end = (j+1) * input_size // self.branch
+                    y = seq[y_start:y_end]
+                    mask[x, y] = 1
+        self.mask = nn.Parameter(mask, requires_grad=False)
+
+    def apply_mask(self):
+        self.dense.weight.data = self.dense.weight.data * self.mask
+
+    def forward(self, x_seq):
+        # x_seq.shape = [T, N, C]
+        T, N = x_seq.shape[:2]
+        padding = torch.zeros([T, N, self.pad], device=x_seq.device)
+        x_seq = torch.cat((x_seq, padding), -1)  # [T, N, Cin]
+        x_seq = F.linear(x_seq, self.dense.weight * self.mask, self.dense.bias)
+        x_seq = x_seq.reshape(T, N, self.out_features, self.branch)
+
+        alpha = torch.sigmoid(self._alpha)
+        vd = torch.zeros_like(x_seq[0])
+        v = torch.rand([N, self.out_features], device=x_seq.device)
+        s = torch.rand_like(v)  # rand: follow the practice of DH-SNN
+        s_seq = torch.empty([T, N, self.out_features], device=s.device)
+        for t in range(T):
+            x = x_seq[t]
+            vd = alpha*vd + (1-alpha) * x
+            y = torch.sum(vd, dim=-1)  # [N, out_features]
+            v, s = plif_update(y, v, s, self.vth, self._beta)
+            s_seq[t] = s
+        return s_seq
+
+
+class LinearDHLIFCheckpointing(nn.Module):
+
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        alpha_initializer='uniform',
+        alpha_low=2,
+        alpha_high=6,
+        beta_initializer='uniform',
+        beta_low=0,
+        beta_high=4,
+        vth=1.,
+        branch=4,
+        mask_share=1,
+        spike_compressor="NullSpikeCompressor",
+    ):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.vth = vth
+        self.branch = branch
+
+        self.mask_share = mask_share
+        self.pad = (in_features//branch*branch + branch - in_features) % branch
+        self.dense = nn.Linear(in_features + self.pad, out_features * branch)
+
+        #sparsity
+        self.sparsity = 1 / branch
+        self.overlap = 1 / branch
+        self._alpha = nn.Parameter(torch.empty([self.out_features, branch]))
+        self._beta = nn.Parameter(torch.empty([self.out_features]))
+
+        self.create_mask()
+
+        if alpha_initializer == 'uniform':
+            nn.init.uniform_(self._alpha, alpha_low, alpha_high)
+        elif alpha_initializer == 'constant':
+            nn.init.constant_(self._alpha, alpha_low)
+
+        if beta_initializer == 'uniform':
+            nn.init.uniform_(self._beta, beta_low, beta_high)
+        elif beta_initializer == 'constant':
+            nn.init.constant_(self._beta, beta_low)
+
+        self.spike_compressor = get_spike_compressor(spike_compressor)
+
+    def create_mask(self):
+        input_size = self.in_features + self.pad  # the real input channels
+        mask = torch.zeros(self.out_features * self.branch, input_size)
+        for i in range(self.out_features // self.mask_share):
+            seq = torch.randperm(input_size)
+            for j in range(self.branch):
+                for k in range(self.mask_share):
+                    x = (i * self.mask_share + k) * self.branch + j
+                    y_start = j * input_size // self.branch
+                    y_end = (j+1) * input_size // self.branch
+                    y = seq[y_start:y_end]
+                    mask[x, y] = 1
+        self.mask = nn.Parameter(mask, requires_grad=False)
+
+    def apply_mask(self):
+        self.dense.weight.data = self.dense.weight.data * self.mask
+
+    @staticmethod
+    def conventional_forward(
+        x_seq, weight, bias, mask, _alpha, _beta, pad, out_features, branch,
+        vth, in_backward
+    ):
+        # x_seq.shape = [T, N, C]
+        T, N = x_seq.shape[:2]
+        padding = torch.zeros([T, N, pad], device=x_seq.device)
+        x_seq = torch.cat((x_seq, padding), -1)  # [T, N, Cin]
+        x_seq = F.linear(x_seq, weight * mask, bias)
+        x_seq = x_seq.reshape(T, N, out_features, branch)
+
+        alpha = torch.sigmoid(_alpha)
+        vd = torch.zeros_like(x_seq[0])
+        v = torch.rand([N, out_features], device=x_seq.device)
+        s = torch.rand_like(v)  # rand: follow the practice of DH-SNN
+        s_seq = torch.empty([T, N, out_features], device=s.device)
+        for t in range(T):
+            x = x_seq[t]
+            vd = alpha*vd + (1-alpha) * x
+            y = torch.sum(vd, dim=-1)  # [N, out_features]
+            v, s = plif_update(y, v, s, vth, _beta)
+            s_seq[t] = s
+        return s_seq
+
+    def forward(self, x_seq):
+        return SNNCheckpointingBlockFunction.apply(
+            self.conventional_forward,
+            self.spike_compressor,
+            x_seq,
+            self.dense.weight,
+            self.dense.bias,
+            self.mask,
+            self._alpha,
+            self._beta,
+            self.pad,
+            self.out_features,
+            self.branch,
+            self.vth,
+        )
+
+
 class PLIFSFNN(nn.Module):
 
     def __init__(self, *args, **kwargs):
@@ -276,6 +465,87 @@ class PGCPLIFSFNN(nn.Module):
             H, H, vth=1., spike_compressor=spike_compressor
         )
         self.dense_3 = LinearPLIF(H, H, vth=1.)
+        self.dense_out = LinearOutputPLIF(H, 20)
+        nn.init.xavier_normal_(self.dense_out.dense.weight)
+        nn.init.constant_(self.dense_out.dense.bias, 0)
+
+    def forward(self, x_seq):
+        x_seq = x_seq.transpose(0, 1)  # [T, N, C]
+        x_seq = self.dense_1(x_seq)
+        x_seq = self.dense_2(x_seq)
+        x_seq = self.dense_3(x_seq)
+        x_seq = self.dense_out(x_seq)  # [T, N, 20]
+
+        logits = F.softmax(x_seq, dim=-1)  # [T, N, 20]
+        return torch.sum(logits[10:], dim=0)  # [N, 20]; discard 1st 10 steps
+
+
+class DHLIFSFNN(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        H = 512
+        self.dense_1 = LinearDHLIF(700, H, vth=1.)
+        self.dense_2 = LinearDHLIF(H, H, vth=1.)
+        self.dense_3 = LinearDHLIF(H, H, vth=1.)
+        self.dense_out = LinearOutputPLIF(H, 20)
+        nn.init.xavier_normal_(self.dense_out.dense.weight)
+        nn.init.constant_(self.dense_out.dense.bias, 0)
+
+    def forward(self, x_seq):
+        x_seq = x_seq.transpose(0, 1)  # [T, N, C]
+        x_seq = self.dense_1(x_seq)
+        x_seq = self.dense_2(x_seq)
+        x_seq = self.dense_3(x_seq)
+        x_seq = self.dense_out(x_seq)  # [T, N, 20]
+
+        logits = F.softmax(x_seq, dim=-1)  # [T, N, 20]
+        return torch.sum(logits[10:], dim=0)  # [N, 20]; discard 1st 10 steps
+
+
+class GCDHLIFSFNN(nn.Module):
+
+    def __init__(self, spike_compressor: str, *args, **kwargs):
+        super().__init__()
+        H = 512
+        self.dense_1 = LinearDHLIFCheckpointing(
+            700, H, vth=1., spike_compressor="NullSpikeCompressor"
+        )
+        self.dense_2 = LinearDHLIFCheckpointing(
+            H, H, vth=1., spike_compressor=spike_compressor
+        )
+        self.dense_3 = LinearDHLIFCheckpointing(
+            H, H, vth=1., spike_compressor=spike_compressor
+        )
+        self.dense_out = LinearOutputPLIFCheckpointing(
+            H, 20, spike_compressor=spike_compressor
+        )
+        nn.init.xavier_normal_(self.dense_out.dense.weight)
+        nn.init.constant_(self.dense_out.dense.bias, 0)
+
+    def forward(self, x_seq):
+        x_seq = x_seq.transpose(0, 1)  # [T, N, C]
+        x_seq = self.dense_1(x_seq)
+        x_seq = self.dense_2(x_seq)
+        x_seq = self.dense_3(x_seq)
+        x_seq = self.dense_out(x_seq)  # [T, N, 20]
+
+        logits = F.softmax(x_seq, dim=-1)  # [T, N, 20]
+        return torch.sum(logits[10:], dim=0)  # [N, 20]; discard 1st 10 steps
+
+
+class PGCDHLIFSFNN(nn.Module):
+
+    def __init__(self, spike_compressor: str, *args, **kwargs):
+        super().__init__()
+        H = 512
+        self.dense_1 = LinearDHLIFCheckpointing(
+            700, H, vth=1., spike_compressor="NullSpikeCompressor"
+        )
+        self.dense_2 = LinearDHLIFCheckpointing(
+            H, H, vth=1., spike_compressor=spike_compressor
+        )
+        self.dense_3 = LinearDHLIF(H, H, vth=1.)
         self.dense_out = LinearOutputPLIF(H, 20)
         nn.init.xavier_normal_(self.dense_out.dense.weight)
         nn.init.constant_(self.dense_out.dense.bias, 0)
